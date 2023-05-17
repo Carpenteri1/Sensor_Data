e24a21c753eaaf42d65ee6ccbb82da672148b369
Carpenteri1 <niclastimle@icloud.com>
2023-05-17 10:59:31 +0200

    encode UUID

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿
 using System.Text;
 using System.Diagnostics;
+using System.Collections;
 
-        // Spawn the sensor simulator process and capture its stdout
+// Spawn the sensor simulator process and capture its stdout
         ProcessStartInfo startInfo = new ProcessStartInfo
         {
             FileName = "./sensor_data.x86_64-apple-darwin",
             RedirectStandardOutput = true,
-            //Arguments = "",
+            //Arguments = "--name test",
             UseShellExecute = false
         };
         Process sensorSimulator = Process.Start(startInfo);
@@ -27,10 +28,19 @@ using System.Diagnostics;
     // Extract the sensor data fields from the packet
     //long timestampMillis = BitConverter.ToInt64(packetBytes, 4);
     DateTime timestamp = DateTimeOffset.FromUnixTimeMilliseconds(timestampMillis).DateTime;
-
+    
     int nameLength = packetBytes[12];
-            string name = Encoding.UTF8.GetString(packetBytes, 13, nameLength);
-            int temperatureOffset = 13 + nameLength;
+    /*byte[] nameBytes = new byte[nameLength];
+    Array.Copy(packetBytes, 13, nameBytes, 0, nameLength);
+    string name = Encoding.UTF8.GetString(nameBytes);
+    */
+    var storePackBytes = packetBytes.Length;
+    Array.Resize(ref packetBytes, 16);
+    Guid uuid = new Guid(packetBytes);
+    string encodedString = uuid.ToString();
+    Array.Resize(ref packetBytes, storePackBytes);
+
+    int temperatureOffset = 13 + nameLength;
             int temperature = BitConverter.ToInt32(packetBytes, temperatureOffset);
             int humidityOffset = temperatureOffset + 3;
             int humidity = BitConverter.ToInt16(packetBytes, humidityOffset);
@@ -38,8 +48,9 @@ using System.Diagnostics;
     // Output the sensor data to log files
     string logFileName = $"_{DateTime.Now:yyyyMMdd}.log";
 
-    string logEntry = $"Timestamp: {timestamp:yyyy-MM-ddTHH:mm:sszzz}, Name: {name}, Temperature: {temperature}°C, Humidity: {humidity}‰";
-            File.AppendAllText(logFileName, logEntry + Environment.NewLine);
+    string logEntry = $"Timestamp: {timestamp:yyyy-MM-ddTHH:mm:sszzz}, Name: {uuid}, Temperature: {temperature}°C, Humidity: {humidity}‰";
+
+    File.AppendAllText(logFileName, logEntry + Environment.NewLine);
 
             Console.WriteLine(logEntry);
         }
