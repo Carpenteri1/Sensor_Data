119e59ffc457ca8ecde79b57faf36c9c8986175d
Carpenteri1 <niclastimle@icloud.com>
2023-05-18 17:44:38 +0200

    add abstract classes

---
+++ AbstractClasses/AbstractJsonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace sensor_data.AbstractClasses
+{
+	public abstract class AbstractJsonModel
+	{
+        protected abstract string Timestamp { get; set; }
+        protected abstract string SensorName { get; set; }
+        protected abstract float? Temperature { get; set; }
+        protected abstract float? Humidity { get; set; }
+	}
+}
+
